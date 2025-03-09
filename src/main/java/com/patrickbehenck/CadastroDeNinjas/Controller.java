335c54d5fc7649ca22ad31b676219ecde64a8b49
PatrickBeh <93169933+PatrickBeh@users.noreply.github.com>
2025-03-09 12:43:49 -0300

    Created a controller

---
+++ src/main/java/com/patrickbehenck/CadastroDeNinjas/Controller.java
@@ -0,0 +1,13 @@
+package com.patrickbehenck.CadastroDeNinjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "This is my first message in this route!";
+    }
+
+}
