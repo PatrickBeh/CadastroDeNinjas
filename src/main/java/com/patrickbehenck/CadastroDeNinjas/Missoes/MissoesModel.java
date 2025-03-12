35bd95874034195c31a08b3ce5339bf91297785e
PatrickBeh <93169933+PatrickBeh@users.noreply.github.com>
2025-03-12 17:26:48 -0300

    feat: Add lombok to the class for abstraction

--- src/main/java/com/patrickbehenck/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/patrickbehenck/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,10 +1,15 @@
 package com.patrickbehenck.CadastroDeNinjas.Missoes;
 import com.patrickbehenck.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,36 +21,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade, List<NinjaModel> ninja) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninja = ninja;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinja() {
-        return ninja;
-    }
-
-    public void setNinja(List<NinjaModel> ninja) {
-        this.ninja = ninja;
-    }
 }
--- src/main/java/com/patrickbehenck/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/patrickbehenck/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,10 +1,14 @@
 package com.patrickbehenck.CadastroDeNinjas.Ninjas;
 import com.patrickbehenck.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 // Entity transforms a common class into a database entity
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,39 +21,6 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Foreign Key ou chave estrangeira
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
 
 }
