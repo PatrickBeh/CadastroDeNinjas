36c2ffe6ea80ef027b3eb59811c3414ab575af96
PatrickBeh <93169933+PatrickBeh@users.noreply.github.com>
2025-04-04 09:00:14 -0300

    feat: Create the controller routes with mock

--- src/main/java/com/patrickbehenck/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/patrickbehenck/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,29 @@
 package com.patrickbehenck.CadastroDeNinjas.Missoes;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+    // GET -- Send a request to show Missions
+    @GetMapping("/list")
+    public String listMissions() {
+        return "Mission listed Successfully";
+    }
+    // POST -- Send a request to create Missions
+    @PostMapping("/create")
+    public String createMission() {
+        return "Mission Created Successfully";
+    }
+    // PUT -- Send a request to update Missions
+    @PutMapping("/update")
+    public String updateMission() {
+        return "Mission Updated Successfully";
+    }
+    // DELETE -- Send a request to delete Missions
+    @DeleteMapping("/delete")
+    public String deleteMission() {
+        return "Mission Deleted Successfully";
+    }
+
+
 }
